6a576c07101aa4f1efef3406687b6d45e769657c
supervueman <supervueman@gmail.com>
2020-02-22 17:43:31 +0400

    Loops

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Card from './Card';
 import AuthControl from './AuthControl';
 
 const App = () => {
+	const items = [1, 2, 3, 4, 5];
+	const list = items.map(el => <li key={el}>{el}</li>)
 	return (
 		<div className="App">
 			<AuthControl />
+			<ul>{list}</ul>
 			<Card name="Rinat" content="Frontend web developer" />
 		</div>
 	);
